d64924d9a561d089b1968c510a04a5c6980ef5a4
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2017-02-20 21:36:43 -0800

    Added handlebars. HTML renders and styling connected

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require("express");
+var handlebars = require("express-handlebars").create({defaultLayout: "main"});
+
 var app = express();
 app.set('port', 3000);
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', function(req, res){
     res.type('application/json');
     response = {'message': 'App is healthy'};
     res.send(response);
+});
+
+app.get('/home', function(req, res){
+    res.render('home');
 });
 
 app.listen(app.get('port'), function(){
